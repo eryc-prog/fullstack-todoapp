f16f5e08a85f66dd955564589be9acd0ab4ed996
Eric Encarnacion <encarnacionerc@gmail.com>
2025-06-04 12:41:03 +0800

    Feat: Add render.yaml

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,10 @@ connectDb();
 const app = express();
 
 // TO DO: create dotenv file and add the port number
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
 
 // Middlewares
 app.use(express.json());
